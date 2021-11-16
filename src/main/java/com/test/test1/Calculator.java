218b998db2e0510d317bed82d8b17aac3744a02b
Dmitriy <dima.ermolovich04@gmail.com>
2021-11-16 20:10:57 +0300

    add functuions
    add functions (+-*/)

---
+++ src/main/java/com/test/test1/Calculator.java
@@ -0,0 +1,20 @@
+package com.test.test1;
+
+public class Calculator {
+    public int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int substractTwoNumbers(int a, int b) {
+        return a - b;
+    }
+
+    public int miltiplicateTwoNumbers(int a, int b){
+        return a * b;}
+
+    public double divideTwoNumbers(double a, double b){
+        return a / b;}
+
+}
+
+
